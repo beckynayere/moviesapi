d46d5d9de476245928a16ec6380a8b8c41ac8466
beckynayere <rebecca.nayere@gmail.com>
2025-03-06 23:47:23 +0300

    config momgodb in app properties

---
+++ src/test/java/com/becky/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becky.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
